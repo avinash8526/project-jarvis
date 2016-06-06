3d6e2d7ae602b56c250609bda89ef1aec596b41e
AVINASH Agrawal <avagrawal@expedia.com>
2016-06-06 15:38:53 +0530

    Adding  our engine

---
+++ utils/ourBrain.js
@@ -0,0 +1,19 @@
+/**
+ * Created by avagrawal on 6/6/16.
+ */
+
+var ourBrain = {};
+
+ourBrain.getCruiseInformation  = function(sessionId,callback,data){
+    };
+
+
+ourBrain.getHotelInformation = function(sessionId,callback,data){
+};
+
+ourBrain.sendMail = function(sessionId,callback,data) {
+
+};
+
+
+module.exports = ourBrain;
